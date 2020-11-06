b3708c38063b19239e9172c2a314d5fe6645be2f
Oscar Gomes dos Santos Junior <oscar.gomes.junior@hotmail.com>
2020-11-06 18:52:36 -0300

    Deletando empresas

--- frontend/src/paginas/inicio/index.js
+++ frontend/src/paginas/inicio/index.js
@@ -1,32 +1,44 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import './style.css';
-import api from '../../services/api'
+import api from '../../services/api';
 
 export default function Inicio(){
     const [empresas, setEmpresas] = useState([]);
 
     const empresaId = localStorage.getItem('empresaId');
-    const razaosocial = localStorage.getItem('empresaRazaosocial');
+    const empresaRazaosocial = localStorage.getItem('empresaRazaosocial');
 
-    useEffect(()=>{
-        api.get('/',{
+    useEffect(() => {
+        api.get('/empresa', {
             headers: {
                 Authorization: empresaId,
             }
         }).then(response => {
             setEmpresas(response.data);
-        });
+        })
     }, [empresaId]);
+
+    async function handleDeletarEmpresas(id){
+        try {
+            await api.delete('empresas/${id}', {
+                headers: {
+                    Authorization: empresaId,
+                }
+            });
+        } catch (err) {
+            alert('erro ao deletar');
+        }
+    }
 
     return (
         <div className="pagina-inicial">
             <div className="conteudo">
-                <header><h3>Bem vindo, {razaosocial}</h3></header>
+                <header><h3>Bem vindo, {empresaRazaosocial}</h3></header>
                 <h1>Empresas</h1><Link className="entrar" to="/login">Login</Link>
                 <ul>
                     {empresas.map(empresa => (
-                        <li key='empresa.id'>
+                        <li key={empresa.id}>
                             <strong>Empresas</strong>
                             <p>{empresa.id}</p>
                             <p>{empresa.razao_social}</p>
@@ -37,7 +49,7 @@ export default function Inicio(){
                             <p>{empresa.numero}</p>
                             <p>{empresa.tipo}</p>
                             <p>{empresa.descricao}</p>
-                            <button type="button">Apagar</button>
+                            <button type="button" onClick={() => handleDeletarEmpresas(empresa.id)}>Apagar</button>
                         </li>
                     ))}
                 </ul>
