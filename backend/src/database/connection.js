39ce32bd4aec25b27686c2151cbcfad4bd7b9fd2
Oscar Gomes dos Santos Junior <oscar.gomes.junior@hotmail.com>
2021-08-08 19:48:10 -0300

    Comentario

--- backend/src/database/connection.js
+++ backend/src/database/connection.js
@@ -1,6 +1,7 @@
 const knex = require('knex');
 const configuration = require('../../knexfile');
 
+/* knexfile.js */
 const connection = knex(configuration.development);
 
 module.exports = connection;
