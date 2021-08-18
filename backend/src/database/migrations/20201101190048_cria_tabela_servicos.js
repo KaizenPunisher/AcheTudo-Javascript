d2436a618e47ff24b50407e85154be73a5df5eaa
Oscar Gomes dos Santos Junior <oscar.gomes.junior@hotmail.com>
2021-08-18 14:38:28 -0300

    Função deletar empresa

--- backend/src/database/migrations/20201101190048_cria_tabela_servicos.js
+++ backend/src/database/migrations/20201101190048_cria_tabela_servicos.js
@@ -2,6 +2,7 @@ exports.up = function(knex) {
     return knex.schema.createTable('servicos', function(table){
         table.increments('id');
         table.string('nome').nullable();
+        
     });
 };
 
