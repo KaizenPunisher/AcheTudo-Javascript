9bb590063b2d8dafaf17d28dcc9da292d0cab8f8
Oscar Gomes dos Santos Junior <oscar.gomes.junior@hotmail.com>
2022-12-21 08:35:36 -0300

    Atualização

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,24 @@
-import React from 'react';
-import './global.css';
-import './mobile.css';
-import AppRoutes from './routes';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <AppRoutes />
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
