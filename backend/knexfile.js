94a13818661dff3bae41161d8ed929b6ce92b566
Oscar Gomes dos Santos Junior <oscar.gomes.junior@hotmail.com>
2023-03-08 00:11:40 -0300

    Correção

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -2,13 +2,12 @@
 module.exports = {
   development: {
     client: 'pg',
-    version: '7.2',
     connection: {
-      host :    'localhost',
-      port :    5432,
-      database: 'db',
-      user:     'postgres',
-      password: '213254'
+      host :     `${process.env.DB_HOST}`,
+      port :     `${process.env.DB_PORT}`,
+      database : `${process.env.DB_NAME}`,
+      user :     `${process.env.DB_USER}`,
+      password : `${process.env.DB_PASS}`
     },
     pool: {
       min: 2,
@@ -22,13 +21,12 @@ module.exports = {
 
   test: {
     client: 'pg',
-    version: '7.2',
     connection: {
-      host :    'localhost',
-      port :    5432,
-      database: 'db',
-      user:     'postgres',
-      password: '213254'
+      host :     `${process.env.DB_HOST}`,
+      port :     `${process.env.DB_PORT}`,
+      database : `${process.env.DB_NAME}`,
+      user :     `${process.env.DB_USER}`,
+      password : `${process.env.DB_PASS}`
     },
     pool: {
       min: 2,
@@ -41,14 +39,22 @@ module.exports = {
   },
 
   staging: {
-    client: 'sqlite3',
+    client: 'pg',
     connection: {
-      filename: './src/database/db.sqlite'
+      host :     `${process.env.DB_HOST}`,
+      port :     `${process.env.DB_PORT}`,
+      database : `${process.env.DB_NAME}`,
+      user :     `${process.env.DB_USER}`,
+      password : `${process.env.DB_PASS}`
+    },
+    pool: {
+      min: 2,
+      max: 10
     },
     migrations: {
+      tableName: 'knex_migrations',
       directory: './src/database/migrations'
-    },
-    useNullAsDefault: true,
+    }
   },
 
   production: {
