cc3aea384578837a21915eec410fd5f1e4f17e60
Oscar Gomes dos Santos Junior <oscar.gomes.junior@hotmail.com>
2023-03-08 17:58:39 -0300

    Desligando BD

--- backend/src/database/dbconfig.js
+++
@@ -1,9 +0,0 @@
-const db = {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    name: process.env.PGDATABASE,
-    user: process.env.PGUSER,
-    pass: process.env.PGPASSWORD
-}
-
-export default db;
