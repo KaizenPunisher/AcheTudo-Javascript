14d31b7b5e9d2fedacc0e38cde41105916770c78
Oscar Gomes dos Santos Junior <oscar.gomes.junior@hotmail.com>
2023-09-15 20:44:49 -0300

    Iniciando projeto NextJS

---
+++ frontend-nextjs/src/app/page.tsx
@@ -0,0 +1,12 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <div className='
+      container mx-auto box-border h-96 h-screen w-auto m-2 border-8 border-white rounded-3xl shadow-md
+      bg-gradient-to-b from-cyan-100 to-cyan-600
+    '>
+    
+    </div>
+  )
+}
---
+++ frontend-nextjs/tailwind.config.ts
@@ -0,0 +1,29 @@
+import type { Config } from 'tailwindcss'
+
+const config: Config = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        botoes: ['var(--font-comfortaa)'],
+        sans: ['var(--font-anton)']
+      },
+
+      borderWidth: {
+        DEFAULT: '1px',
+        '0': '0',
+        '2': '2px',
+        '3': '3px',
+        '4': '4px',
+        '6': '6px',
+        '8': '20px',
+      },
+    },
+  },
+  plugins: [],
+}
+export default config
